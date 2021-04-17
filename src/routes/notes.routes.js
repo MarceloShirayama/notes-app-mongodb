feeb656e7c66af0ef0d27c2d98da7d16d602c306
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2021-04-17 13:42:45 -0300

    ✨ auth notes routes

--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -1,4 +1,5 @@
 const { Router } = require('express');
+const { isAuthenticated } = require('../helpers/auth');
 
 const router = Router();
 
@@ -8,17 +9,17 @@ const {
 } = require('../controllers/notes.controller');
 
 // New note
-router.get('/notes/add', renderNoteForm);
+router.get('/notes/add', isAuthenticated, renderNoteForm);
 
-router.post('/notes/new-note', createNewNote);
+router.post('/notes/new-note', isAuthenticated, createNewNote);
 
 // Get all notes
-router.get('/notes', renderNotes);
+router.get('/notes', isAuthenticated, renderNotes);
 
 // Edit notes
-router.get('/notes/edit/:id', renderEditForm);
+router.get('/notes/edit/:id', isAuthenticated, renderEditForm);
 
-router.put('/notes/edit/:id', updateNote);
+router.put('/notes/edit/:id', isAuthenticated, updateNote);
 
 // Delete notes
 router.delete('/notes/delete/:id', deleteNote);
